db964e8b6dad88efd0b16cff8bcd9bf9f016dbc6
Guilhem Saurel <guilhem.saurel@gmail.com>
2015-10-05 17:18:05 +0200

    nouveau split du settings \o/

--- edirtic/edirtic/wsgi.py
+++ edirtic/edirtic/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "edirtic.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "edirtic.local_settings" if os.path.isfile('edirtic/local_settings.py') else "edirtic.settings")
 
 application = get_wsgi_application()
--- edirtic/manage.py
+++ edirtic/manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "edirtic.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "edirtic.local_settings" if os.path.isfile('edirtic/local_settings.py') else "edirtic.settings")
 
     from django.core.management import execute_from_command_line
 
