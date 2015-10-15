964c9a68698b09a6e2c496d52c9d36f17f3ee5da
jhuet <julien.huet@smartjog.com>
2015-10-16 00:00:14 +0200

    Add angularjs

---
+++ edirtic/maincourante/static/scripts/maincourante/app.js
@@ -0,0 +1,24 @@
+'use strict';
+
+angular.module('maincouranteApp', [
+  	'ngRoute',
+    'ngResource',
+  	'edir.maincourante.controllers',
+  	'edir.maincourante.entities'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+      	templateUrl: '/static/view/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
+
+angular.module('maincouranteApp').config(function($interpolateProvider) {
+    $interpolateProvider.startSymbol('{$');
+    $interpolateProvider.endSymbol('$}');
+});
