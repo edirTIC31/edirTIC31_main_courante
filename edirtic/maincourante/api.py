efff8d773f44a297afc7660497d7f89d4912e355
Élie Bouttier <elie@bouttier.eu>
2015-10-16 11:25:22 +0200

    fix opérateur

--- edirtic/maincourante/api.py
+++ edirtic/maincourante/api.py
@@ -5,5 +5,8 @@ from .models import Message
 class MessageResource(ModelResource):
     class Meta:
         queryset = Message.objects.all()
-        allowed_methods = ['get']
+        allowed_methods = ['get', 'post']
 
+    def dehydrate(self, bundle):
+        bundle.data['operateur'] = bundle.request.user
+        return bundle
