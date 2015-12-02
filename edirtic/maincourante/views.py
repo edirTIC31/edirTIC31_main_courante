d2496f85e68ebf3e32d4ee29e21cc268ab05d942
Guilhem Saurel <guilhem.saurel@gmail.com>
2015-12-02 15:00:32 +0100

    plus de braces

--- edirtic/maincourante/views.py
+++ edirtic/maincourante/views.py
@@ -1,5 +1,5 @@
-from braces.views import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.exceptions import PermissionDenied
 from django.core.urlresolvers import reverse
 from django.http import Http404, JsonResponse
