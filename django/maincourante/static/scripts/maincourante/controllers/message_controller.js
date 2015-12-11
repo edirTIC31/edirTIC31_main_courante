d328f64b0e40d596c19b4b88bd1b673b919a6570
jhuet <julien.huet@smartjog.com>
2015-12-11 20:59:16 +0100

    Add reply button

--- django/maincourante/static/scripts/maincourante/controllers/message_controller.js
+++ django/maincourante/static/scripts/maincourante/controllers/message_controller.js
@@ -34,13 +34,13 @@ function prepareMainController($scope, Message, MessageManager, IndicatifManager
                     response.body = $scope.response;
                     MessageManager.add(response).then(
                         function(resp) {
-                            $scope.body = null;
-                            $scope.response = null;
-                            $scope.messages.push(resp);
-                        },
-                        function(errorPayload) {
-                            alert("Erreur lors de l'ajout de la reponse");
-                        });
+                        $scope.body = null;
+                        $scope.response = null;
+                        $scope.messages.push(resp);
+                    },
+                    function(errorPayload) {
+                        alert("Erreur lors de l'ajout de la reponse");
+                    });
                 }
                 $scope.messages.push(msg);
                 $scope.sender = null;
@@ -48,6 +48,7 @@ function prepareMainController($scope, Message, MessageManager, IndicatifManager
                 $scope.body = null;
                 $scope.$broadcast('angucomplete-alt:clearInput', 'sender');
                 $scope.$broadcast('angucomplete-alt:clearInput', 'receiver');
+                focus('onNewMessage');
             },
             function(errorPayload) {
                 alert("Erreur lors de l'ajout du nouveau message");
@@ -120,11 +121,32 @@ function prepareMainController($scope, Message, MessageManager, IndicatifManager
     $scope.deleteMessage = function(message) {
         var modalInstance = $modal.open({
             animation: $scope.animationsEnabled,
-            templateUrl: 'myModalContent.html',
-            controller: 'ModalInstanceCtrl',
+            templateUrl: 'modal-delete-message.html',
+            controller: 'ModalDeleteMessageCtrl',
             resolve: {
                 message: function () {
                     return message;
+                }
+            }
+        });
+        modalInstance.result.then(function (selectedItem) {
+            $scope.selected = selectedItem;
+        }, function () {
+            $log.info('Modal dismissed at: ' + new Date());
+        });
+    };
+
+    $scope.replyMessage = function(message) {
+        var modalInstance = $modal.open({
+            animation: $scope.animationsEnabled,
+            templateUrl: 'modal-reply-message.html',
+            controller: 'ModalReplyMessageCtrl',
+            resolve: {
+                message: function () {
+                    return message;
+                },
+                messages: function () {
+                    return $scope.messages;
                 }
             }
         });
@@ -146,7 +168,7 @@ function prepareMainController($scope, Message, MessageManager, IndicatifManager
 // Please note that $modalInstance represents a modal window (instance) dependency.
 // It is not the same as the $uibModal service used above.
 
-angular.module('edir.maincourante.controllers').controller('ModalInstanceCtrl', function ($scope, $modalInstance, MessageManager, message) {
+angular.module('edir.maincourante.controllers').controller('ModalDeleteMessageCtrl', function ($scope, $modalInstance, MessageManager, message) {
 
     $scope.ok = function () {
         MessageManager.delete(message, $scope.suppressionMessage).then(
@@ -156,6 +178,31 @@ angular.module('edir.maincourante.controllers').controller('ModalInstanceCtrl', 
          },
          function(errorPayload) {
             alert("Erreur lors de la suppression du message");
+        });
+    };
+
+    $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
+    };
+});
+
+angular.module('edir.maincourante.controllers').controller('ModalReplyMessageCtrl', function ($scope, $modalInstance, MessageManager, Message, message, messages) {
+    $scope.message = message;
+    $scope.ok = function () {
+        var response = new Message();
+        response.receiver = message.sender;
+        response.sender = message.receiver;
+        response.evenement = EVENEMENT;
+        response.body = $scope.response;
+        MessageManager.add(response).then(
+            function(resp) {
+                $scope.body = null;
+                $scope.response = null;
+                messages.push(resp);
+                $modalInstance.close();
+            },
+            function(errorPayload) {
+                alert("Erreur lors de l'ajout de la reponse");
         });
     };
 
