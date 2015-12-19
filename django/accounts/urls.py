8f2e5393052c61d8b6959cb96d368378b5b7ca6e
Élie Bouttier <elie@bouttier.eu>
2015-12-19 18:14:36 +0100

    séparation login avec et sans password

--- django/accounts/urls.py
+++ django/accounts/urls.py
@@ -4,8 +4,9 @@ from django.contrib.auth.models import User
 
 
 urlpatterns = [
-    url(r'^login/$', 'accounts.views.login', name='login'),
-    url(r'^login/(?P<username>[\w.@+-]+)$', 'accounts.views.passless_login', name='passless-login'),
+    url(r'^login/$', 'accounts.views.login_operator', name='login'),
+    url(r'^login/(?P<username>[\w.@+-]+)$', 'accounts.views.login_operator', name='login-ope'),
+    url(r'^secure-login/$', 'accounts.views.login_administrator', name='login-admin'),
     url(r'^create/$', 'accounts.views.create_user', name='create-user'),
     url(r'^logout/$', 'django.contrib.auth.views.logout',
         {'next_page': '/'}, name='logout'),
--- django/accounts/views.py
+++ django/accounts/views.py
@@ -9,34 +9,45 @@ from django.contrib.auth.models import User
 from accounts.forms import CreateUserForm
 
 
-def login(request, *args, **kwargs):
+def login_operator(request, username=None):
 
     form = CreateUserForm()
 
+    if username:
+
+        user = get_object_or_404(User, username=username)
+
+        if user.is_superuser:
+
+            messages.error(request, "Il faut un mot de passe pour se connecter "
+                                    "avec cet utilisateur ;-)")
+            return redirect('login')
+
+        user.backend = 'django.contrib.auth.backends.ModelBackend'
+        contrib_login(request, user)
+
+        redirect_to = request.GET.get(REDIRECT_FIELD_NAME, reverse('login'))
+
+        return redirect(redirect_to)
+
     c = {
         'create_user_form': form,
-        'users': User.objects.all(),
+        'operators': User.objects.filter(is_superuser=False, is_active=True),
         'next': request.GET.get(REDIRECT_FIELD_NAME, ''),
     }
 
-    return contrib_login_view(request, *args, template_name='accounts/login.html',
+    return render(request, 'accounts/login_operator.html', c)
+
+def login_administrator(request, *args, **kwargs):
+
+    c = {
+        'administrators': User.objects.filter(is_superuser=True),
+        'next': request.GET.get(REDIRECT_FIELD_NAME, ''),
+    }
+
+    return contrib_login_view(request, *args,
+            template_name='accounts/login_administrator.html',
             extra_context=c, **kwargs)
-
-def passless_login(request, username):
-
-    user = get_object_or_404(User, username=username)
-
-    if user.is_superuser:
-        messages.error(request, "Il faut un mot de passe pour se connecter "
-                                "avec cet utilisateur ;-)")
-        return redirect('login')
-
-    redirect_to = request.GET.get(REDIRECT_FIELD_NAME, reverse('login'))
-
-    user.backend = 'django.contrib.auth.backends.ModelBackend'
-    contrib_login(request, user)
-
-    return redirect(redirect_to)
 
 def create_user(request):
 
@@ -49,9 +60,9 @@ def create_user(request):
     if not form.is_valid():
         messages.error(request, 'Ce nom d’utilisateur existe déjà.')
         return redirect(redirect_to) # FIXME
-    assert(not User.objects.filter(username=username).exists())
 
     username = form.cleaned_data['username']
+    assert(not User.objects.filter(username=username).exists())
 
     user = User.objects.create_user(username)
     user.backend = 'django.contrib.auth.backends.ModelBackend'
