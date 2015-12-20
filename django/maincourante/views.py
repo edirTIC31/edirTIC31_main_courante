edirTIC31/edirTIC31_main_courante

commit f2a0396825e15088291384ab4a66ea2853ccbf85

messages: conflit avec django.contrib.messages
du coup rennomage en msgs

django/maincourante/views.py
@@ -57,7 +57,7 @@ def evenement_cloture(request, evenement):
 def evenement_report(request, evenement):
 
     return render(request, 'maincourante/evenement_report.html', {
-        'messages': MessageThread.objects.filter(evenement=evenement),
+        'msgs': MessageThread.objects.filter(evenement=evenement),
     })
 
 
@@ -66,7 +66,7 @@ def evenement_live(request, evenement):
 
     messages = MessageThread.objects.filter(evenement=evenement).all()[:10]
     return render(request, 'maincourante/evenement_live.html', {
-        'messages': messages,
+        'msgs': messages,
     })
 
 
@@ -101,7 +101,7 @@ def message_add(request, evenement, message=None):
         return redirect(reverse('add-message', args=[evenement.slug]))
 
     return render(request, 'maincourante/message_add.html', {
-        'messages': MessageThread.objects.filter(evenement=evenement).all()[:10],
+        'msgs': MessageThread.objects.filter(evenement=evenement).all()[:10],
         'add_form': form,
         'edit_form': edit_form,
         'delete_form': delete_form,
@@ -176,7 +176,7 @@ def message_last(request, evenement):
 
     c = {
         'evenement': evenement,
-        'messages': messages,
+        'msgs': messages,
         'show_deleted': deleted,
         'show_history': history,
         'show_tools': tools,

django/accounts/templates/accounts/login_administrator.html
@@ -31,6 +31,7 @@
 {% block js_end %}
 {{ block.super }}
 <script type="text/javascript">
+/* to disable anchor (anchor break focus) */
 $('.admin-link').on('click', function(e) {
     e.preventDefault();
 });

django/maincourante/templates/maincourante/evenement_live.html
@@ -12,7 +12,7 @@
 </div>
 
 <div id="messages-content">
-{% render_messages messages show_deleted=False show_history=False %}
+{% render_messages msgs show_deleted=False show_history=False %}
 </div>
 {% endblock %}
 

django/maincourante/templates/maincourante/evenement_report.html
@@ -19,5 +19,5 @@
         <p><a href="javascript:window.print()"><span class="glyphicon glyphicon-print"></span>&#160;Imprimer</a>
     </div>
 </div>
-{% render_messages messages %}
+{% render_messages msgs %}
 {% endblock %}

django/maincourante/templates/maincourante/message_add.html
@@ -51,7 +51,7 @@
 </div>
 
 <div id="messages-content">
-{% render_messages messages show_tools=True show_deleted=True %}
+{% render_messages msgs show_tools=True show_deleted=True %}
 </div>
 
 <div class="modal" id="confirm-delete" tabindex="-1" role="dialog" aria-hidden="true">

django/maincourante/templates/maincourante/tags/messages.html
@@ -1,5 +1,5 @@
 {% load message_tags %}
-{% for message in messages %}
+{% for message in msgs %}
 {% if show_deleted or not message.deleted %}
 {% render_message message show_tools=show_tools show_history=show_history %}
 {% endif %}

commit 3146b412b5539e4289a3bdc7a1036e46f015a102

fix regression introduced in f2a03968

django/maincourante/views.py
@@ -176,7 +176,7 @@ def message_last(request, evenement):
 
     c = {
         'evenement': evenement,
-        'msgs': messages,
+        'messages': messages,
         'show_deleted': deleted,
         'show_history': history,
         'show_tools': tools,