bd73ce4ab75af5ef7ccaf0a27b426abe3deb91d9
Guilhem Saurel <guilhem.saurel@gmail.com>
2019-03-09 22:55:36 +0100

    fix logout

--- django/accounts/urls.py
+++ django/accounts/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from django.contrib.auth import logout
+from django.contrib.auth.views import LogoutView
 
 from . import views
 
@@ -7,5 +7,5 @@ urlpatterns = [
     url(r'^login/$', views.login_operator, name='login'),
     url(r'^login/(?P<username>[\w.@+-]+)$', views.login_operator, name='login-ope'),
     url(r'^secure-login/$', views.login_administrator, name='login-admin'),
-    url(r'^logout/$', logout, {'next_page': 'list-evenements'}, name='logout'),
+    url(r'^logout/$', LogoutView.as_view(next_page='list-evenements'), name='logout'),
 ]
